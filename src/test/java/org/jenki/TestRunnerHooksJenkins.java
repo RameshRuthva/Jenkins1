36de35d3e196e950e7d53dba4fcf3e57de73689c
RameshRuthva <rameshraja.eee14@gmail.com>
2019-06-10 10:38:46 +0530

    add json

--- src/test/java/org/jenki/TestRunnerHooksJenkins.java
+++ src/test/java/org/jenki/TestRunnerHooksJenkins.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Feature/AddTariffJenkins.feature",dryRun=false,strict=true,monochrome=true,tags= {"@us1","@regression"},glue="org.jenki",plugin="html:target")
+@CucumberOptions(features="src/test/resources/Feature/AddTariffJenkins.feature",dryRun=false,strict=true,monochrome=true,tags= {"@us1","@regression"},glue="org.jenki",plugin= {"json:src/test/resources/report.json"})
 
 
 public class TestRunnerHooksJenkins 
